f7884bc13e8ceaa2d9ec5cf32166aba3a5dde1c4
Damian Ludwig <damian.ludwig@uni-siegen.de>
2020-03-30 13:03:13 +0200

    Change from texlive-scheme-full to customized install
    TeXlive full is /very/ big and one usually need only a small part of it.

--- texlive/build.sh
+++ texlive/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
-NAME=dev
+NAME=texlive
 RELEASE=f$(rpm -E %fedora)
 
 toolbox rm --force $NAME || true
@@ -8,6 +8,12 @@ toolbox create --release $RELEASE -c $NAME
 
 toolbox run -c "$NAME" sh -exec '\
 sudo dnf -y install glibc-langpack-en
-sudo dnf -y install texlive-scheme-basic rubber
+sudo dnf -y install texlive-collection-basic \
+	texlive-collection-latex \
+	texlive-collection-mathscience \
+	texlive-collection-pictures \
+	texlive-collection-langgerman \
+	texlive-collection-latexrecommended \
+	rubber
 sudo dnf clean all
 '
